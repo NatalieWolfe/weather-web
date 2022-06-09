405c5e22dd5a67410f3d3d0bcb73839d91286875
Natalie Wolfe <natalie@lifewanted.com>
2022-06-08 22:39:18 -0700

    Basic template support.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder};
+use handlebars::Handlebars;
+use serde_json::json;
 
 #[get("/")]
-async fn index(_params: web::Path<()>) -> impl Responder {
-  format!("Hello!")
+async fn index(hb: web::Data<Handlebars<'_>>) -> impl Responder {
+  HttpResponse::Ok().body(hb.render("index", &json!({})).unwrap())
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  HttpServer::new(|| App::new().service(index))
+  let mut handlebars = Handlebars::new();
+  handlebars
+    .register_templates_directory(".html", "./static/templates")
+    .unwrap();
+  let handlebars_ref = web::Data::new(handlebars);
+
+  HttpServer::new(move || {
+      App::new()
+        .wrap(middleware::Compress::default())
+        .wrap(middleware::Logger::default())
+        .app_data(handlebars_ref.clone())
+        .service(index)
+    })
     .bind(("::0", 8080))?
     .run()
     .await
